7f9a7d7a3a0127c340437f2634f3514dead6a58d
Antonio <antonio_hataki@hotmail.com>
2026-04-17 17:09:00 -0300

    Alteração

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World! teste");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World! teste");
+        int x = 10;
+    }
+}
